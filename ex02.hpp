8c2a8cda6709463744fa66357b22ab5adebc63ee
RamPrin <89806764+RamPrin@users.noreply.github.com>
2022-03-07 14:50:23 +0300

    Update ex02.hpp

--- ex02.hpp
+++ ex02.hpp
@@ -2,6 +2,7 @@
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
 
+template<class T>
 public class Calculator {
 private:
     int num1, num2;

aa434ba05c064a48ddea1bd0e7ba370a8b86b31b
RamPrin <89806764+RamPrin@users.noreply.github.com>
2022-03-07 14:39:33 +0300

    Update ex01.hpp

--- ex01.hpp
+++ ex01.hpp
@@ -2,7 +2,9 @@
 //change the below code
 // the function below now only works with integers, change it to work with any type
 // also fill out the body
-void multiples (int& sum, int x, int n)
+template<class WhatKind>
+void multiples (WhatKind& sum, WhatKind x, int n)
 {
-
+  sum = 1;
+  for(int i = 1; i <= n; i++)sum += i * x;
 }
